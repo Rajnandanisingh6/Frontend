175d8651e0cdcb695cae8254bb2a434086b8367a
Rajnandanisingh6 <rajnandanisingh07610761@gmail.com>
2025-10-10 10:02:46 +0530

    Into To Js

---
+++ intro_To_Js/script.js
@@ -0,0 +1,9 @@
+console.log("Hello")
+
+// var let const
+var x=11;
+var y="Namste"
+console.log("x")
+console.log("y")
+
+
---
+++ intro_To_Js/varletconst.js
@@ -0,0 +1,27 @@
+// var x=20; //redecalare reassin kar sakte 
+// console.log(x);
+// var x=30;
+// x=50;
+// console.log(x);
+
+// let y=40; //reassin kar sakte hai but redeclare nahi
+// console.log(y);
+// //let y; can't do this
+
+// y=66;
+// console.log(y);
+
+// const z=30; // na hi redecale kar sakte hai aur na hi reassin
+// // const z can't do this
+
+// //z=40; can't do this
+// console.log(z);
+
+//var -- functional scope hai
+//let , const -- block scope
+
+var a=22;
+let b=32;
+const c=12;
+
+console.log(a,b,c);
